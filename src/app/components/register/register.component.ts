4eb54f7ef955939245dfd75ae9681d7709edd9c9
Six3306 <six3306@gmail.com>
2021-08-21 12:30:31 -0500

    Agregando modificacion de prueba en registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -16,26 +16,26 @@ import { FirebaseService } from 'src/app/services/firebase/firebase.service';
 
 
 export class RegisterComponent implements OnInit {
-  
+
   /**
    * @param formRegister es el register 
    */
-  formRegister : FormGroup; 
+  formRegister: FormGroup;
 
   /**
    * @param submitted es la variable que indica si existe algun error en el formulario de existir su valor se vuelve True
    */
-  submitted=false; 
-  
+  submitted = false;
+
   /**
    * @param roles son los roles existentes para el registro
    */
-  rols: string[] = ['Profesor', 'Alumno']; 
-  
+  rols: string[] = ['Profesor', 'Alumno'];
+
   /**
    * @param alertComponentRef componente de alerta todavia no esta terminado
    */
-  alertComponentRef:MatDialogRef<AlertComponent>; 
+  alertComponentRef: MatDialogRef<AlertComponent>;
 
   /**
    * Constructor de la clase del registro en el se instancian las variables importadas
@@ -47,42 +47,29 @@ export class RegisterComponent implements OnInit {
    */
   constructor(
     private api: APIService,
-    private formbuilder : FormBuilder,
+    private formbuilder: FormBuilder,
     private router: Router,
     private dialog: MatDialog,
     private firebase: FirebaseService,
     private snackBar: MatSnackBar,
-  ) {  
+  ) {
     this.formRegister = this.formbuilder.group({
-      username : ['',Validators.required],
-      lastNameFather : ['', Validators.required],
-      lastNameMother : ['',Validators.required],
-      yearsold:['',Validators.required],
-      password : ['',Validators.required],
-      rol: ['',Validators.required],
-      email : ['',Validators.required],
-      confirmPassword: ['',Validators.required],
+      username: ['', Validators.required],
+      lastNameFather: ['', Validators.required],
+      lastNameMother: ['', Validators.required],
+      yearsold: ['', Validators.required],
+      password: ['', Validators.required],
+      rol: ['', Validators.required],
+      email: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
     })
   }
-  
+
 
   /**
    * Inicializacion del controlador
    */
-  ngOnInit() {
-
-
-  }
-
-  /**
-   * Metodo para mandar a llamar a la alerta no esta terminado 
-   */
-  openAddFileDialog() {
-    this.alertComponentRef = this.dialog.open(AlertComponent,{
-      hasBackdrop: false,
-      autoFocus: true
-    });
-  }
+  ngOnInit() { }
 
 
   /**
@@ -95,99 +82,94 @@ export class RegisterComponent implements OnInit {
   /**
    * Permite realizar el registro del usuario
    */
-  register(){ 
-    let user: User;
-    user={
-      username:this.formRegister.get("username").value+" "+this.formRegister.get("lastNameFather").value+" "+this.formRegister.get("lastNameMother").value,
-      password:this.formRegister.get("password").value,
-      email: this.formRegister.get("email").value,
-      age: this.formRegister.get("yearsold").value,
-      type:this.formRegister.get("rol").value,
-      activated:null,
-      id:null
-    } 
+  register() {
+    var caP = ["{", "}", ".", ",", ";", ":", "[", "]", "(", ")", "-", "_", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "?", "!", "|", "&", "%", "#", "$", "/", "<", ">", "*"];
+    var name = this.formRegister.get("username").value;
+    var lastnameF = this.formRegister.get("lastNameFather").value;
+    var lastnameM = this.formRegister.get("lastNameMother").value;
+    var username = name + " " + lastnameF + " " + lastnameM;
+    var band = true;
 
-    //valida si email de usuario existe
-    this.api.verifyEmail(user.email).subscribe(response=>{
-      
-console.log(response['data']);
- 
-      if(response['data']==false){//no existe
-        this.api.register(user).subscribe(response =>{
-
-          //añadiendo a firebase
-          var band = this.firebase.addUser(user);
-    
-          //verificando si todo salio bien
-          if(band){
-            localStorage.setItem("user", JSON.stringify(user));
-            // localStorage.setItem("token", response.token) 
-            this.router.navigateByUrl("Menu");
-          }
-  
-        }, error=>{
-          console.log(error);
-        });
-      }else{//si existe
-        this.router.navigateByUrl("Register");
+    for (let i = 0; i < caP.length; i++) {
+      if (username.includes(caP[i])) {
+        band = false;
       }
-      
-    });
-    
+    }
+
+    if(name.trim()=="" || lastnameF.trim()=="" || lastnameM.trim()=="" ){
+      band = false;
+    }
+
+    if (band) {
+      let user: User;
+      user = {
+        username: username,
+        password: this.formRegister.get("password").value,
+        email: this.formRegister.get("email").value,
+        age: this.formRegister.get("yearsold").value,
+        type: this.formRegister.get("rol").value,
+        activated: null,
+        id: null
+      }
+
+      //valida si email de usuario existe
+      this.api.verifyEmail(user.email).subscribe(response => {
+
+        if (response['data'] == false) {//no existe
+          this.api.register(user).subscribe(response => {
+
+            //añadiendo a firebase
+            var band = this.firebase.addUser(user);
+
+            //verificando si todo salio bien
+            if (band) {
+              localStorage.setItem("user", JSON.stringify(user));
+              // localStorage.setItem("token", response.token) 
+              this.router.navigateByUrl("Menu");
+            }
+
+          }, error => {
+            console.log(error);
+          });
+        } else {//si existe
+          this.router.navigateByUrl("Register");
+        }
+
+      });
+    }else{
+      this.openCustomerSnackBarLesson();
+    }
   }
 
   /**
    * Envia y comprueba que los campos del form esten bien
    * @returns retorna un elemento vacio para terminar la funcion 
    */
-  sendRegister(){
-    
-    this.submitted=true;
-    if(this.formRegister.invalid || (this.formRegister.get("password").value != this.formRegister.get("confirmPassword").value ) ){
-      // console.log(this.formRegister.value)
+  sendRegister() {
+    this.submitted = true;
+    if (this.formRegister.invalid || (this.formRegister.get("password").value != this.formRegister.get("confirmPassword").value)) {
       this.openCustomerSnackBarLesson();
       return;
     }
     //registrando
     this.register();
-    this.submitted=false;
-
-    
+    this.submitted = false;
   }
   /**
    * Funcion que redirige al login
    */
-  Login(){
+  Login() {
     this.router.navigateByUrl("Login")
   }
 
-  /**
-   * Funcion para probar el guardado de datos en firebase
-   */
-  testFirebase(){
-    let user:User = new User ("Alex",12,"luis_pesar@hotmail.com","Alejandro1998a","Alumno","",false)
-    let id;
-    this.firebase.getUsers().subscribe(response=>{
-      for (let index = 0; index < response.length; index++) {
-        const element = response[index];
-        // console.log(element.payload.doc.data())
-        // console.log(element.payload.doc.id)
-        id=element.payload.doc.id
-        // user=element.payload.doc.data
-        
-      }
-    })
-    this.firebase.updateUser("4nbQ5UTx4YWWI17w2gH2", user) 
+  //para ir directamente a la ventana del login
+  loginP() {
+    this.router.navigateByUrl("");
   }
 
-  //para ir directamente a la ventana del login
-  loginP(){
-    this.router.navigateByUrl("");
-  } 
-
   //para mostrar un mensaje emergente de que algo salio mal al capturar los datos y registrarse
-  openCustomerSnackBarLesson(){
-    return this.snackBar.openFromComponent(CustomSnackBarComponentRegister, {duration: 4000});
+  openCustomerSnackBarLesson() {
+    return this.snackBar.openFromComponent(CustomSnackBarComponentRegister, { duration: 4000 });
   }
 
 }
@@ -197,4 +179,4 @@ console.log(response['data']);
   selector: 'custom-snackbar',
   template: `<span style='color: #ee0303dc;'><strong>Error al Registrar, Favor de Verificar la Información Ingresada</strong></span>`
 })
-export class CustomSnackBarComponentRegister{}
+export class CustomSnackBarComponentRegister { }
